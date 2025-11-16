871a4b7191e221d6cdf342920bd83812df9e26b5
Aleksandr Bykov <alexoidb@yandex.ru>
2025-11-16 22:08:30 +0300

    PROJ: ch1

--- PROJ/lib/mongo.sh
+++ PROJ/lib/mongo.sh
@@ -4,9 +4,9 @@ set -o nounset   # abort on unbound variable
 set -o pipefail  # don't hide errors within pipes
 
 docker compose -f /opt/conf/mongo.docker-compose.yml up -d
-sleep 5
-docker exec mongo1 mongosh --eval 'rs.initiate({_id: "rs0", members: [{ _id: 0, host: "database:27017" }, { _id: 1, host: "database:27018" }, { _id: 2, host: "database:27019" }]})'
 sleep 10
+docker exec mongo1 mongosh --eval 'rs.initiate({_id: "rs0", members: [{ _id: 0, host: "database:27017" }, { _id: 1, host: "database:27018" }, { _id: 2, host: "database:27019" }]})'
+sleep 15
 docker exec mongo1 mongosh --eval 'db = db.getSiblingDB("rocketchat"); db.testCollection.insertOne({message: "Hello World"});'
 docker exec mongo1 mongosh admin --eval 'db = db.getSiblingDB("admin"); db.createUser({ user: "rocket-app", pwd: "rocketpassword", roles: [{ role: "dbOwner", db: "rocketchat" }] })'
 
