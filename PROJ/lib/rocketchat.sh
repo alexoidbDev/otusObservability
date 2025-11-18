c841461e8115e4b3995a2c39a8600e9662d1d977
Aleksandr Bykov <aleksandr.bykov@krm.ilimgroup.ru>
2025-11-18 17:20:03 +0300

    PROJ: ch3

--- PROJ/lib/rocketchat.sh
+++ PROJ/lib/rocketchat.sh
@@ -4,7 +4,7 @@ set -o errexit   # abort on nonzero exit status
 set -o nounset   # abort on unbound variable
 set -o pipefail  # don't hide errors within pipes
 
-docker pull mongo:6.0.18 # for verification
+# docker pull mongo:6.0.18 # for verification
 
 ## https://github.com/RocketChat/Docker.Official.Image/blob/master/env.example
 source /opt/conf/rocketchat-env.sh
@@ -12,4 +12,4 @@ source /opt/conf/rocketchat-env.sh
 ## https://github.com/RocketChat/Docker.Official.Image/blob/master/compose.yml
 docker compose -f /opt/conf/rocketchat.docker-compose.yml up -d
 
-docker logs -f rocketchat
+# docker logs -f rocketchat
